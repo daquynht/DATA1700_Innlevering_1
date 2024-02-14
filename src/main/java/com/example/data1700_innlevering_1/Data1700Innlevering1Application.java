4c204d960bf30b3da02f5b702a724590a45f383a
daquynht <117162734+daquynht@users.noreply.github.com>
2024-02-14 17:02:54 +0100

    Initial commit

---
+++ src/main/java/com/example/data1700_innlevering_1/Data1700Innlevering1Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700_innlevering_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Innlevering1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Innlevering1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1700_innlevering_1/Data1700Innlevering1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700_innlevering_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Innlevering1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
